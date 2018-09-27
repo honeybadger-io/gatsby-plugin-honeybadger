f1680b127de171727dcbefe9596181d67519d9be
Nate Shoemaker <nate@hintmedia.com>
2018-09-27 11:27:55 -0700

    Add console warning if no api key is present

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -1,6 +1,11 @@
 const Honeybadger = require('honeybadger-js')
 
 exports.onClientEntry = function(_, { apiKey }) {
+  if (!apiKey) {
+    console.warn('gatsby-plugin-honeybadger needs an API key to be configured properly {url for documentation}')
+    return
+  }
+
   Honeybadger.configure({
     apiKey,
     environment: process.env.NODE_ENV
